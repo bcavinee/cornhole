c8f057350063a876a37d82ac0173babf44da19ae
bcavinee <51139252+bcavinee@users.noreply.github.com>
2022-09-09 15:35:29 -0400

    procfile

--- cornhole/settings.py
+++ cornhole/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-#ALLOWED_HOSTS = ['cornholeleaderboard.herokuapp.com']
+ALLOWED_HOSTS = ['cornholeleaderboard.herokuapp.com']
 
 
 # Application definition
