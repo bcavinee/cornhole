e6c4fc13af8b0c3264c5b9073e4d041887ee8037
bcavinee <51139252+bcavinee@users.noreply.github.com>
2022-09-09 14:59:06 -0400

    settings

--- cornhole/settings.py
+++ cornhole/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 import os
+from decouple import config
+import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -21,12 +23,14 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-@6r5nbkv3dv#n7_fanc7+lafb3r#sanrf=%*#)07s5b_qcli2@'
+#SECRET_KEY = 'django-insecure-@6r5nbkv3dv#n7_fanc7+lafb3r#sanrf=%*#)07s5b_qcli2@'
+
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['cornholeleaderboard.herokuapp.com']
 
 
 # Application definition
